f366a3e8311cec6555d499d3401e1b80742ff7c7
Kevin Duraj <kduraj@gmail.com>
2017-10-05 13:51:18 -0700

    2017-10-05 13:51

--- src/main/scala/TotalLinksInDallas.scala
+++ src/main/scala/TotalLinksInDallas.scala
@@ -14,8 +14,10 @@ object TotalLinksInDallas {
 
     def main(args: Array[String]) {
 
-        //val conf = new SparkConf(true).setAppName("TotalLinksInDallas")
-        val conf = new SparkConf(true).set("spark.cassandra.connection.host", "10.0.0.1")
+        val conf = new SparkConf(true)
+                        .setAppName("TotalLinksInDallas")
+                        .set("spark.cassandra.connection.host", "10.0.0.1")
+
         //val sc = new SparkContext("spark://69.13.39.46:7077", "cloud1", conf)
         val sc = new SparkContext(conf)
 
