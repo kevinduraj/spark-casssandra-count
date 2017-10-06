838cca970573f2094f00bd8625a536938362eaed
Kevin Duraj <kevin.x.duraj.-nd@disney.com>
2017-10-05 20:12:42 -0700

    2017-10-05 20:12

--- src/main/scala/TopDomains.scala
+++ src/main/scala/TopDomains.scala
@@ -1,5 +1,9 @@
 import com.datastax.spark.connector._
 import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql.SQLContext
+import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark.sql.cassandra._
+
 
 case class domain(name: String, count: BigInt)
 case class Row(name:String, count:BigInt)
@@ -26,24 +30,37 @@ object TopDomains {
   /*-------------------------------------------------------------------------------------*/  
   def get_largest_visited_domains(table_name: String, size: Int): Unit = {
         
-        val conf = new SparkConf(true)
-                        .setAppName("TopDomains")
-                        .set("spark.cassandra.connection.host", "10.0.0.1")
-        //val sc = new SparkContext("spark://69.13.39.46:7077", "cloud1", conf)
-        val spark = new SparkContext(conf)
-        
-        val table1 = spark.cassandraTable("cloud1", table_name)
-        val total = table1.cassandraCount()
-        println("Total " +  table_name + " = " + total)
+//        val conf = new SparkConf(true)
+//                        .setAppName("TopDomains")
+//                        .set("spark.cassandra.connection.host", "10.0.0.1")
+//        //val sc = new SparkContext("spark://69.13.39.46:7077", "cloud1", conf)
+//        val spark = new SparkContext(conf)
+//
+//        val table1 = spark.cassandraTable("cloud1", table_name)
+//        val total = table1.cassandraCount()
+//        println("Total " +  table_name + " = " + total)
+//
+//
+//        val table2 = table1.spanBy(row => (row.getString("domain")))
+//        table2.groupByKey.count
+//        table2.take(100).foreach(println)
+//        println(table2.getClass)
+      /*-------------------------------------------------------------------------------------*/
+      import org.apache.spark.sql.SparkSession
 
-        val table2 = table1.spanBy(row => (row.getString("domain")))
-        table2.groupByKey.count
-        table2.take(100).foreach(println)
-        println(table2.getClass)
+      val spark = SparkSession
+          .builder()
+          .appName("TopDomains")
+          .config("spark.cassandra.connection.host", "10.0.0.1")
+          .getOrCreate()
 
+      val df1 = spark.read
+          .cassandraFormat("vdomain", "cloud1", "Cassandra Cluster")
+          .load()
 
+            df1.show(100, false)
 
-        //val result1 = table2.count()
+      //val result1 = table2.count()
         //println(result1)
 
         //val result = table1.select("domain").groupBy("domain")
