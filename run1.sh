610293bb3411aeaf98b555a29a565ed2ac3a6ede
Kevin Duraj <kduraj@gmail.com>
2017-10-05 18:24:50 -0700

    2017-10-05 18:24

--- run1.sh
+++ run1.sh
@@ -6,6 +6,15 @@ if [ "$1" == "0" ]; then
 #-------------------------------------------------------------#
 elif [ "$1" == "1" ]; then
 spark-submit    \
+  --class "TopDomains"      \
+  --master local[16]        \
+  --driver-memory   32G     \
+  --executor-memory 16G     \
+  target/scala-2.11/spark-cassandra-count_2.11-1.0.jar $2 $3 
+  
+
+elif [ "$1" == "2" ]; then
+spark-submit    \
   --class "TotalLinks"      \
   --master local[16]        \
   --driver-memory   32G     \
@@ -15,12 +24,13 @@ spark-submit    \
 #-------------------------------------------------------------#
 else
 
-  echo "+--------------------------------"
-  echo "| 0. Build Assembly              "
-  echo "| 1. Total Links                 "
-  echo "+--------------------------------"
-  echo "| 4. Clear                       "
-  echo "+--------------------------------"
+  echo "+---------------------------------------------"
+  echo "| 0. Build Assembly                           "
+  echo "| 1. ./run1.sh 1 vdomain 750                  "
+  echo "| 2. Total Links                              "
+  echo "+---------------------------------------------"
+  echo "| 4. Clear                                    "
+  echo "+---------------------------------------------"
 
 fi
 
